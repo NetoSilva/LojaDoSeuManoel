2001947910b68314a02a2abc15fd51593cb741b7
Florentino Neto <netosilva1994@gmail.com>
2024-10-16 18:37:58 -0300

    feat: adicionado classe de request para o método ProcessarPedidos.

---
+++ LojaDoSeuManoel/OrdersAPI/Requests/ProcessarPedidosRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PedidosAPI.Requests
+{
+    public partial class ProcessarPedidosRequest
+    {
+        [JsonPropertyName("pedidos")]
+        public PedidoDTO[]? PedidosDTO { get; set; }
+    }
+
+    public partial class PedidoDTO
+    {
+        [JsonPropertyName("pedido_id")]
+        public int PedidoId { get; set; }
+
+        [JsonPropertyName("produtos")]
+        public ProdutoDTO[]? ProdutosDTO { get; set; }
+    }
+
+    public partial class ProdutoDTO
+    {
+        [JsonPropertyName("produto_id")]
+        public string? ProdutoId { get; set; }
+
+        [JsonPropertyName("dimensoes")]
+        public DimensoesDTO? DimensaoDTO { get; set; }
+    }
+
+    public partial class DimensoesDTO
+    {
+        [JsonPropertyName("altura")]
+        public double Altura { get; set; }
+
+        [JsonPropertyName("largura")]
+        public double Largura { get; set; }
+
+        [JsonPropertyName("comprimento")]
+        public double Comprimento { get; set; }
+    }
+}
