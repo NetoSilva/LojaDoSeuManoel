9ef7e2d11b9fdf30408b2339c3490251b3fe7583
Florentino Neto <netosilva1994@gmail.com>
2024-10-16 18:32:11 -0300

    feat: Instalado o AutoMapper e adicionado classe MappingProfile para fazer o mapeamento das DTOs para objetos de Dominio.

---
+++ LojaDoSeuManoel/OrdersAPI/Mappings/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using LojaDoSeuManoel.Domain.Models;
+using PedidosAPI.Requests;
+
+namespace PedidosAPI.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<PedidoDTO, Pedido>()
+                .ForMember(dest => dest.IdPedido, map => map.MapFrom(src => src.PedidoId))
+                .ForMember(dest => dest.Produtos, map => map.MapFrom(src => src.ProdutosDTO))
+                .ReverseMap();
+
+            CreateMap<ProdutoDTO, Produto>()
+                .ForMember(dest => dest.IdProduto, map => map.MapFrom(src => src.ProdutoId))
+                .ForMember(dest => dest.Dimensao, map => map.MapFrom(src => src.DimensaoDTO))
+                .ReverseMap();
+
+            CreateMap<DimensoesDTO, Dimensao>()
+                .ForMember(dest => dest.Altura, map => map.MapFrom(src => src.Altura))
+                .ForMember(dest => dest.Largura, map => map.MapFrom(src => src.Largura))
+                .ForMember(dest => dest.Comprimento, map => map.MapFrom(src => src.Comprimento))
+                .ReverseMap();
+        }
+    }
+}
